5179999f0efdf9b678b9e2a4ab73741efd712d54
zengyonghao <yonghaozeng@qq.com>
2023-01-03 17:01:45 +0800

    去掉main方法

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# go_func_sugar
+# go-sugar
+
+some function sugar to go-developer
+
+一些语法糖给到go开发者,以实现开发时更加顺畅
+
+起因是其他语言转go后,会发现一些日常快捷使用的函数在这里需要重写,每次都重写就太麻烦了
+
+欢迎提pr共建这个库,功在当天,利在春秋呀
+
+###目前已实现:
+InArray(val,array) 判断单个值是否在某个数组或切片中,是则返回下标,否则返回-1
---
+++ sugar.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"reflect"
+)
+
+// InArray
+// Determines whether a single value is contained in an array or slice
+// 判断单个数值是否包含在数组或切片中
+func InArray(val interface{}, array interface{}) (index int) {
+	index = -1
+
+	switch reflect.TypeOf(array).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(array)
+		for i := 0; i < s.Len(); i++ {
+			if reflect.DeepEqual(val, s.Index(i).Interface()) == true {
+				index = i
+				return
+			}
+		}
+	}
+	return
+}
